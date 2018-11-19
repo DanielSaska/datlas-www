10ce8da4adc3785b84a6b6de467b374698d5a052
Daniel Saska <daniel@saska.io>
2018-11-19 18:52:26 +0000

    Frontend now implements basic functionality of the data atlas

--- src/app/tag-details/tag-details.component.ts
+++ src/app/tag-details/tag-details.component.ts
@@ -2,18 +2,33 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
+import { MatPaginator } from '@angular/material/paginator';
 
 import cfg from '../../config';
 
-export interface RecordingDetails {
-	human_id: string;
-	datetime: Date;
-	duration: Number;
-	tags: string[];
-	_id: string;
+export interface BehSeqElem {
+	details: string[];
+	name: string;
+	img: string;
 }
 
-export interface AnaAddon {
+export interface SummaryEntry {
+	icon: string;
+	name: string;
+	value: string;
+	list: Boolean;
+}
+
+export interface Summary {
+	entries: SummaryEntry[];
+	sequence: BehSeqElem[];
+	youtube_reg_dfof: string;
+}
+
+
+export interface AnalysisData {
 	title: string;
 	subtitle: string;
 	html: string;
@@ -21,17 +36,44 @@ export interface AnaAddon {
 	priority: Number;
 }
 
-export interface AnalysisDetails {
-	addons: AnaAddon[];
-}
-
-export interface TagDetails {
-	name: string;
-	recordings: RecordingDetails[];
-	analysis: AnalysisDetails;
+export interface Visualization {
 	_id: string;
+	exp_name: string;
+	commit: string;
+	data: AnalysisData;
+
 }
 
+export interface Tag {
+	name: string;
+	description: string;
+	n_recordings: number;
+	analysis: string[];
+	_id: string;
+
+	ana: Visualization[];
+}
+
+export interface Recording {
+	_id: string;
+	commit: string;
+	data_types: string[];
+	summary: Summary;
+	custom: Object;
+	tags: string[];
+	human_id: string;
+	experiment: string;
+}
+
+export interface Response {
+	recordings: Recording[];
+	start: Number;
+	next: Number;
+	pg_size: Number;
+	max_pg: Number;
+	n_entries: Number;
+
+}
 
 @Component({
   selector: 'app-tag-details',
@@ -39,44 +81,95 @@ export interface TagDetails {
   styleUrls: ['./tag-details.component.css']
 })
 export class TagDetailsComponent implements OnInit {
-	displayedColumns: string[] = ['id', 'date', 'tags', 'duration'];
-	tag_name : string = "";
-	count : number = 0;
-	plotly_velocity: any;
-	analysis_addons: any;
-	tag_details: TagDetails;
+	dispCols: string[];
+	userCols: object[] = [];
+	recordings: Response = null;
+	loading: boolean = true;
+	tag: Tag;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
-	constructor(private http: HttpClient,private route: ActivatedRoute,private _sanitizer: DomSanitizer) {
+	constructor(private http: HttpClient,private route: ActivatedRoute,private _sanitizer: DomSanitizer, private snackBar: MatSnackBar) {
 		this.route.params.subscribe(params => {
-			this.http.get<TagDetails>(cfg.apiUrl+"/v1/tag/"+params.id+"/details").subscribe((res: TagDetails) => {
-				this.tag_name = res.name;
-				this.count = res.recordings.length;
-
-				for (let r of res.recordings) {
-					r.datetime = new Date(r.datetime);
+			this.http.get<Tag>(cfg.apiUrl+"/v1/tag/"+params.id).subscribe(async (res: Tag) => {
+				res.ana = [];
+				for (let a of res.analysis) {
+					res.ana.push(null);
 				}
-				this.tag_details = res;
 
-				if (res.analysis && res.analysis.addons) {
-					let addons = [];
-					for (let a of res.analysis.addons) {
-						let addon = {
-							html: this._sanitizer.bypassSecurityTrustHtml(a.html),
-							plot: a.plot,
-							title: a.title,
-							subtitle: a.subtitle,
-							priority: a.priority
-						}
-						addons.push(addon);
+				this.tag = res;
+
+				if (res.analysis) {
+					for (let ai = 0; ai < res.analysis.length; ++ai) {
+						let a = res.analysis[ai];
+						let url = cfg.apiUrl+"/v1/tag/analysis/"+a;
+						let r = await this.http.get<Visualization>(url).toPromise();
+						res.ana[ai] = r;
 					}
-					this.analysis_addons = addons
 				}
-
+				if (res.n_recordings > 0) {
+					let url = cfg.apiUrl+"/v1/tag/"+this.tag._id+"/recordings/0";
+					this.http.get<Response>(url).subscribe((res: Response) => {
+						this.updateTable(res);
+					});
+				}
+				this.loading = false;
+			}, (err: any) => {
+				this.loading = false;
+				this.snackBar.open("Error loading the resource", "OK", {
+					duration: 15000,
+					verticalPosition: "top",
+				});
 			});
 		});
 	}
 
+	updateTable(res) {
+		this.dispCols = ['id', 'experiment', 'tags'];
+		this.userCols = [];
+		this.recordings = null;
+
+		for (let r of res.recordings) {
+			if (r.summary && r.summary.entries) {
+				for (let e of r.summary.entries) {
+					if (e.list) {
+						if (!r.custom) { r.custom = {}; }
+						if (!r.custom[e.name]) { r.custom[e.name] = e.value.toString(); }
+						else {r.custom[e.name] += ", " + e.value.toString(); };
+
+						if (!this.dispCols.includes("usr"+e.name)) {
+							this.userCols.push({def:"usr"+e.name,name:e.name});
+							this.dispCols.splice(this.dispCols.length-2,0,"usr"+e.name);
+						}
+					}
+				}
+			}
+		}
+		//console.log(this.userCols);
+		//console.log(this.dispCols);
+		this.recordings = res;
+		//console.log(res);
+	}
+
+	flipPage() {
+		let start = this.paginator.pageIndex * <number>this.recordings.pg_size;
+		this.recordings = null;
+		this.http.get<Response>(cfg.apiUrl+"/v1/tag/"+this.tag._id+"/recordings/"+start.toString()).subscribe((res: Response) => {
+			this.updateTable(res);
+		});
+	}
+
+	ngAfterViewInit() {
+		this.paginator.page
+			.pipe(
+				tap(() => this.flipPage())
+			)
+			.subscribe();
+	}
+
+
+
 	ngOnInit() {
 	}
+
 
 }
